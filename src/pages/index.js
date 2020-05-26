9912109c30597d43e826eca64f89e3e7ea5f8020
MelvinDerrick <35552014+MelvinDerrick@users.noreply.github.com>
2020-05-27 00:31:28 +0530

    Add precompiled assets for Heroku

---
+++ src/pages/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import ScriptTag from "react-script-tag";
+
+const MainPage = () => {
+    return (
+       <h2>Hello Im in Page 2</h2>
+    );
+};
+
+export default MainPage;
